86b92dbabba27c84bafb16a41739c17923ec9398
shygosh <shygosh@proton.me>
2024-09-17 11:40:44 +0700

    sched/cass: Return idle cpu immediately once found
    CASS is a busy man, and he should do something more worthy of his time
    than comparing which idle cpu has the lowest idle exit latency.
    Let's remove idle exit latency condition and simplify things a bit.
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -27,7 +27,6 @@
 
 struct cass_cpu_cand {
 	int cpu;
-	unsigned int exit_lat;
 	unsigned long cap;
 	unsigned long util;
 };
@@ -89,10 +88,6 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     !cass_cmp(b->cpu, smp_processor_id())))
 		goto done;
 
-	/* Prefer the CPU with lower idle exit latency */
-	if (cass_cmp(b->exit_lat, a->exit_lat))
-		goto done;
-
 	/* Prefer the previous CPU */
 	if (cass_eq(a->cpu, prev_cpu) || !cass_cmp(b->cpu, prev_cpu))
 		goto done;
@@ -106,9 +101,8 @@ done:
 static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt)
 {
 	/* Initialize @best such that @best always has a valid CPU at the end */
-	struct cass_cpu_cand cands[2], *best = cands, *curr;
-	struct cpuidle_state *idle_state;
-	bool has_idle = false;
+	struct cass_cpu_cand cands[2], *best = cands;
+	int this_cpu = raw_smp_processor_id();
 	unsigned long p_util = rt ? 0 : task_util_est(p);
 	int cidx = 0, cpu;
 	int adj = p->signal->oom_score_adj;
@@ -125,36 +119,15 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	for_each_cpu_and(cpu, valid_mask, cpu_active_mask) {
 		/* Use the free candidate slot for @curr */
 		struct cass_cpu_cand *curr = &cands[cidx];
-		struct cpuidle_state *idle_state;
 		struct rq *rq = cpu_rq(cpu);
 
 		/*
 		 * Check if this CPU is idle. For sync wakes, always treat the
 		 * current CPU as idle.
 		 */
-		if ((sync && cpu == smp_processor_id()) || idle_cpu(cpu)) {
-			/* Discard any previous non-idle candidate */
-			if (!has_idle) {
-				best = curr;
-				cidx ^= 1;
-			}
-			has_idle = true;
-
-			/* Nonzero exit latency indicates this CPU is idle */
-			curr->exit_lat = 1;
-
-			/* Add on the actual idle exit latency, if any */
-			idle_state = idle_get_state(cpu_rq(cpu));
-			if (idle_state)
-				curr->exit_lat += idle_state->exit_latency;
-		} else {
-			/* Skip non-idle CPUs if there's an idle candidate */
-			if (has_idle)
-				continue;
-
-			/* Zero exit latency indicates this CPU isn't idle */
-			curr->exit_lat = 0;
-		}
+		if ((sync && cpu == this_cpu && rq->nr_running == 1) || 
+			(idle_cpu(cpu) && !cpu_isolated(cpu)))
+			return cpu;
 
 		/* Get this CPU's capacity and utilization */
 		curr->cpu = cpu;
