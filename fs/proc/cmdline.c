fb506121d189ec645214b71b1357959e24ff3639
TEW404 <84270286+tew080@users.noreply.github.com>
2025-04-04 16:15:38 +0700

    Fix

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -13,7 +13,8 @@ static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-
+	seq_puts(m, new_command_line);
+	seq_putc(m, '\n');
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
 		seq_putc(m, '\n');
